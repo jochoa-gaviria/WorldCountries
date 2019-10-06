a729067adf6b2c08a80c067fa930e9b13745d8fc
JUAN DAVID OCHOA GAVIRIA <juanochoa220427@correo.itm.edu.co>
2019-10-06 17:27:35 -0500

    Show the image on country details

--- WorldCountries.Prism/WorldCountries.Prism/ViewModels/CountryPageViewModel.cs
+++ WorldCountries.Prism/WorldCountries.Prism/ViewModels/CountryPageViewModel.cs
@@ -1,17 +1,31 @@
-﻿using Prism.Commands;
+﻿using Newtonsoft.Json;
+using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Navigation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using WorldCountries.Common.Helpers;
+using WorldCountries.Common.Models;
 
 namespace WorldCountries.Prism.ViewModels
 {
     public class CountryPageViewModel : ViewModelBase
     {
+        private CountriesResponse _country;
         public CountryPageViewModel(INavigationService navigationService) : base(navigationService)
         {
             Title = "Details";
+        }
+        public CountriesResponse Country
+        {
+            get => _country;
+            set => SetProperty(ref _country, value);
+        }
+        public override void OnNavigatedTo(INavigationParameters parameters)
+        {
+            base.OnNavigatedTo(parameters);
+            Country = JsonConvert.DeserializeObject<CountriesResponse>(Settings.Country);
         }
     }
 }
