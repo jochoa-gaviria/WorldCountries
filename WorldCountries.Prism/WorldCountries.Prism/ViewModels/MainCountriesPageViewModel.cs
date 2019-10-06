02567e68475f0a4bab398dc6d46eeb06a1855ab4
JUAN DAVID OCHOA GAVIRIA <juanochoa220427@correo.itm.edu.co>
2019-10-06 15:37:21 -0500

    Completed the funtionality to show the countries without internet.

--- WorldCountries.Prism/WorldCountries.Prism/ViewModels/MainCountriesPageViewModel.cs
+++ WorldCountries.Prism/WorldCountries.Prism/ViewModels/MainCountriesPageViewModel.cs
@@ -3,6 +3,7 @@ using Prism.Navigation;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Threading.Tasks;
 using WorldCountries.Common.Helpers;
 using WorldCountries.Common.Models;
 using WorldCountries.Common.Service;
@@ -46,13 +47,35 @@ namespace WorldCountries.Prism.ViewModels
             var connection = await _apiService.CheckConnectionAsync(url);
             if (!connection)
             {
-                IsRunning = false;
-                await App.Current.MainPage.DisplayAlert("Error", "Check the internet connection.", "Accept");
-                return;
+                if (Settings.Countries == null)
+                {
+                    IsRunning = false;
+                    await App.Current.MainPage.DisplayAlert("Sorry", "There's not data on memory, check your internet connection", "Accept");
+                    return;
+                }
+                var response = LoadPersistenceCountries();
+                ShowCountries(response);
+
+            }
+            else
+            {
+                var response = await _apiService.GetCountriesAsync(url, "/rest", "/v2", "/all");
+                ShowCountries(response);
             }
 
-            var response = await _apiService.GetCountriesAsync(url, "/rest", "/v2", "/all");
+        }
 
+        private Response<CountriesResponse> LoadPersistenceCountries()
+        {
+            var CountriesKept = JsonConvert.DeserializeObject<List<CountriesResponse>>(Settings.Countries);
+            return new Response<CountriesResponse>
+            {
+                IsSuccess = true,
+                Result = CountriesKept
+            };
+        }
+        private async void ShowCountries(Response<CountriesResponse> response)
+        {
             if (!response.IsSuccess)
             {
                 IsRunning = false;
