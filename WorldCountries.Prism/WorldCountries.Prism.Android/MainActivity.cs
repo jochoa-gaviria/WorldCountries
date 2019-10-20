0852f44506125f8b905925d6ee3426e8329bfb34
JUAN DAVID OCHOA GAVIRIA <juanochoa220427@correo.itm.edu.co>
2019-10-19 21:35:04 -0500

    Add icons and splash

--- WorldCountries.Prism/WorldCountries.Prism.Android/MainActivity.cs
+++ WorldCountries.Prism/WorldCountries.Prism.Android/MainActivity.cs
@@ -10,7 +10,8 @@ using Plugin.Permissions;
 
 namespace WorldCountries.Prism.Droid
 {
-    [Activity(Label = "WorldCountries.Prism", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "World Countries", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme",
+        MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
